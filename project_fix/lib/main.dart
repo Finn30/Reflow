5c7855ea8933b67a384e1d4c118cb8da1feec3a1
LAERESU <yusril03ibtida@gmail.com>
2025-01-04 12:23:30 +0800

    autentikasi registrasi

--- project_fix/lib/main.dart
+++ project_fix/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:project_fix/src/features/welcome/welcome_screen.dart';
 import 'package:project_fix/src/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
