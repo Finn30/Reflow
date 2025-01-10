d2024b1ea13aa4b5a3f78921ff361353ebbf197a
Finn30 <kevin304.kv@gmail.com>
2025-01-10 23:18:04 +0800

    ets

---
+++ project_fix/lib/src/features/home screen/home_screen.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:project_fix/src/constant/image_string.dart';
+import 'package:project_fix/src/features/about%20us/aboutus_screen.dart';
+import 'package:project_fix/src/features/car%20guide/carguide_screen.dart';
+import 'package:project_fix/src/features/feedback/feedback_screen.dart';
+import 'package:project_fix/src/features/invite%20friends/invitefriends_screen.dart';
+import 'package:project_fix/src/features/language%20switch/languageswitch_screen.dart';
+import 'package:project_fix/src/features/my%20profile/myprofile_screen.dart';
+import 'package:project_fix/src/features/my%20trip/mytrip_screen.dart';
+import 'package:project_fix/src/features/my%20wallet/mywallet_screen.dart';
+import 'package:project_fix/src/features/user%20manual/usermanual_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gridwiz'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            AppBar(
+              leading: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(Icons.menu),
+              ),
+              title: Text('Menu'),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(logo_gridwiz),
+                          radius: 30.0,
+                        ),
+                        SizedBox(width: 16.0),
+                        Text(
+                          'Kevin',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // UserAccountsDrawerHeader(
+                  //   decoration: BoxDecoration(
+                  //     color: Colors.blue,
+                  //   ),
+                  //   accountName: Text('Kevin'),
+                  //   accountEmail: null,
+                  //   currentAccountPicture: CircleAvatar(
+                  //     backgroundImage: AssetImage(logo_gridwiz),
+                  //   ),
+                  // ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('My Profile'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyProfileScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.luggage),
+                    title: Text('My Trip'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyTripScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.account_balance_wallet),
+                    title: Text('My Wallet'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyWalletScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.menu_book),
+                    title: Text('User Manual'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserManualScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.directions_car),
+                    title: Text('Car Guide'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CarGuideScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.share),
+                    title: Text('Invite Friends'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => InviteFriendsScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.feedback),
+                    title: Text('Feedback'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FeedbackScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text('About Us'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AboutUsScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.language),
+                    title: Text('Language Switch'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LanguageSwitchScreen()));
+                    },
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[300]!)),
+              ),
+              child: Row(
+                children: [
+                  Image(image: AssetImage(logo_gridwiz), width: 40, height: 40),
+                  SizedBox(width: 10),
+                  Text(
+                    'Gridwiz',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
---
+++ project_fix/lib/src/features/login/login_screen.dart
@@ -0,0 +1,357 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:project_fix/src/features/home%20screen/home_screen.dart';
+import 'package:project_fix/src/features/register/register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool useEmail = false;
+  bool obscurePassword = true;
+  TextEditingController inputController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isChecked = false;
+  String activeIcon = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void login() async {
+    try {
+      final input = inputController.text.trim();
+      final password = passwordController.text;
+
+      if (input.isEmpty || password.isEmpty) {
+        showError("Email atau Kata Sandi tidak boleh kosong.");
+        return;
+      }
+
+      // Mengacu ke koleksi pengguna di Firestore
+      final usersCollection = FirebaseFirestore.instance.collection('user');
+
+      // Mencari email dalam database
+      final querySnapshot =
+          await usersCollection.where('email', isEqualTo: input).get();
+
+      if (querySnapshot.docs.isEmpty) {
+        showError("Email tidak terdaftar. Silakan daftar terlebih dahulu.");
+        return;
+      }
+
+      // Mendapatkan dokumen pertama yang sesuai dengan email
+      final userDoc = querySnapshot.docs.first;
+      final userData = userDoc.data();
+
+      // Memeriksa kecocokan password
+      if (userData['password'] == password) {
+        // Jika login berhasil
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      } else {
+        showError("Password salah. Silakan coba lagi.");
+        return;
+      }
+    } catch (e) {
+      showError("Terjadi kesalahan: $e");
+    }
+  }
+
+  void showError(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Error"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/img/gridwiz.jpg',
+                height: 200,
+              ),
+              SizedBox(height: 20),
+              if (!useEmail)
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      DropdownButton<String>(
+                        value: '+62',
+                        items: ['+62', '+1', '+44', '+91', '+81'].map((code) {
+                          return DropdownMenuItem(
+                            value: code,
+                            child: Text(code, style: TextStyle(fontSize: 16)),
+                          );
+                        }).toList(),
+                        onChanged: (value) {},
+                        underline: SizedBox(),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextFormField(
+                          controller: inputController,
+                          keyboardType: TextInputType.phone,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                          ],
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Silakan masukkan nomor telepon',
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          inputController.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              else
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: inputController,
+                          keyboardType: TextInputType.emailAddress,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.deny(
+                                RegExp(r'[^a-zA-Z0-9@._-]')),
+                          ],
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Silakan masukkan email',
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          inputController.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: obscurePassword,
+                  decoration: InputDecoration(
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          obscurePassword = !obscurePassword;
+                        });
+                      },
+                      child: Icon(
+                        obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Silakan masukkan kata sandi',
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isChecked = !isChecked;
+                      });
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.black),
+                            color: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          width: 16,
+                          height: 16,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: isChecked ? Colors.blue : Colors.transparent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: Colors.black, fontSize: 12),
+                        children: [
+                          TextSpan(
+                              text: 'Saya telah membaca dan setuju dengan '),
+                          TextSpan(
+                            text: 'Perjanjian Pengguna',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                          TextSpan(text: ' dan '),
+                          TextSpan(
+                            text: 'Kebijakan Privasi',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: isChecked ? login : null,
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    backgroundColor: Colors.grey[400],
+                  ),
+                  child: Text("Login"),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterScreen()),
+                      );
+                    },
+                    child: Text("Daftar", style: TextStyle(color: Colors.blue)),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text("Lupa kata sandi?",
+                        style: TextStyle(color: Colors.grey[700])),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: activeIcon == 'phoneEmail'
+                              ? Colors.blue
+                              : Colors.grey),
+                      color: Colors.white,
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        useEmail ? Icons.phone : Icons.email,
+                        size: 40,
+                      ),
+                      color: activeIcon == 'phoneEmail'
+                          ? Colors.blue
+                          : Colors.black,
+                      onPressed: () {
+                        setState(() {
+                          useEmail = !useEmail;
+                          activeIcon = 'phoneEmail';
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: activeIcon == 'apple'
+                              ? Colors.blue
+                              : Colors.grey),
+                      color: Colors.white,
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.apple, size: 40),
+                      color: activeIcon == 'apple' ? Colors.blue : Colors.black,
+                      onPressed: () {
+                        setState(() {
+                          activeIcon = 'apple';
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
